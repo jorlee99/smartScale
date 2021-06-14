c86805cef79e580f46d39e83ea24959f0def2f12
Jordan Lee <jordan@pantheondesign.ca>
2021-06-14 16:02:57 -0700

    Added submit button on enter

--- app/src/main/java/com/example/scale/MainActivity.java
+++ app/src/main/java/com/example/scale/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.scale;
 
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,7 +10,12 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.content.DialogInterface.OnClickListener;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.MultiAutoCompleteTextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String[] FOODS = new String[]{
@@ -27,14 +33,44 @@ public class MainActivity extends AppCompatActivity {
         editText.setAdapter(adapter);
     
         Button btn_ResetInputs = (Button)findViewById(R.id.Reset);
+        Button btn_SubmitInput = (Button)findViewById(R.id.Submit);
+
+        ListView listView = (ListView) findViewById(R.id.leftListView);
+        ArrayList<String> list = new ArrayList<String>();
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, list);
+
+        ListView masslistView = (ListView) findViewById(R.id.rightListView);
+        ArrayList<String> list2 = new ArrayList<String>();
+        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, list2);
 
         btn_ResetInputs.setOnClickListener(new View.OnClickListener(){
           @Override
           public void onClick(View v) {
               editText.setText("");
               weight.setText("");
+//              InputMethodManager imm =(InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//              imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
           }
         });
+
+        btn_SubmitInput.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v){
+               String foods = editText.getText().toString();
+
+               list.add(foods);
+               listView.setAdapter(arrayAdapter);
+               arrayAdapter.notifyDataSetChanged();
+
+               String mass = weight.getText().toString();
+
+               list2.add(mass);
+               masslistView.setAdapter(arrayAdapter2);
+               arrayAdapter2.notifyDataSetChanged();
+           }
+        });
+
+
 //        AutoCompleteTextView foodList = findViewById(R.id.Foodlist);
 
     }
